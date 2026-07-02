89a46afa7c4baac37b6806b1042c999b3a3f3531
code-plagiarism-detector <noreply@github.com>
2026-07-03 01:40:29 +0200

    Cache unibo-oop-projects/Student-Project-OOP-21-Fragozzi-Sangiorgi-Sternini-Vuksan-hideseek

---
+++ repositories/github.com/unibo-oop-projects/Student-Project-OOP-21-Fragozzi-Sangiorgi-Sternini-Vuksan-hideseek/src/main/java/hidenseek/App.java
@@ -0,0 +1,16 @@
+package hidenseek;
+
+import hidenseek.model.Launcher;
+import javafx.application.Application;
+
+/** Main application entry-point. */
+
+public final class App {
+
+    private App() {
+    }
+    public static void main(final String[] args) {
+        Application.launch(Launcher.class, args);
+       
+    }
+}
