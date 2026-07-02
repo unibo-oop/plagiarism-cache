95f24eb277b9eb5ab8a15bc71604d6c94555c553
code-plagiarism-detector <noreply@github.com>
2026-07-03 01:34:06 +0200

    Cache unibo-oop-projects/Student-Project-OOP21-Babboni-Bartolini-DeTuglie-Romeo-map

---
+++ repositories/github.com/unibo-oop-projects/Student-Project-OOP21-Babboni-Bartolini-DeTuglie-Romeo-map/src/controller/Main.java
@@ -0,0 +1,20 @@
+package controller;
+
+/**
+ * 
+ * Start of the program
+ *
+ */
+public final class Main {
+  private Main() {
+  }
+
+  /**
+   * Start the game loop
+   * 
+   * @param args the argument to be passed
+   */
+  public static void main(final String[] args) {
+    new GameLoop();
+  }
+}
