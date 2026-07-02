4c6a9514d538435eab74d19f7845c3b6e35a56cc
code-plagiarism-detector <noreply@github.com>
2026-07-03 01:28:18 +0200

    Cache unibo-oop-projects/Student-Project-OOP20-Carboni-DeNicol-Domi-Ongaro-GooseGame

---
+++ repositories/github.com/unibo-oop-projects/Student-Project-OOP20-Carboni-DeNicol-Domi-Ongaro-GooseGame/src/main/java/application/App.java
@@ -0,0 +1,18 @@
+package application;
+
+import controller.menu.MenuController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+       final MenuController menu = new MenuController();
+       menu.start();
+    }
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+}
---
+++ repositories/github.com/unibo-oop-projects/Student-Project-OOP20-Carboni-DeNicol-Domi-Ongaro-GooseGame/src/main/java/application/AppLauncher.java
@@ -0,0 +1,11 @@
+package application;
+
+public final class AppLauncher {
+
+    private AppLauncher() {
+    }
+
+    public static void main(final String[] args) {
+        App.main(args);
+    }
+}
